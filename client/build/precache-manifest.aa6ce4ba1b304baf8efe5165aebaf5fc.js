8176ce3e6b3c573a66b1b627280bfb9408075504
Tom Ward <wardtommy59@gmail.com>
2021-02-08 10:32:51 -0500

    Dockerized & updated Readme

---
+++ client/build/precache-manifest.aa6ce4ba1b304baf8efe5165aebaf5fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64446156c8f8144dd00d79d6c7a3fe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964b5d31f94f9a0a23ec",
+    "url": "/static/css/main.3161d2c1.chunk.css"
+  },
+  {
+    "revision": "8ae19f383213068fa591",
+    "url": "/static/js/2.dd432fa5.chunk.js"
+  },
+  {
+    "revision": "27e28cccc1ec422e68316fd29acd149a",
+    "url": "/static/js/2.dd432fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964b5d31f94f9a0a23ec",
+    "url": "/static/js/main.c4d6caea.chunk.js"
+  },
+  {
+    "revision": "bc31d366474b0812baf0",
+    "url": "/static/js/runtime-main.3b2b8d44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
